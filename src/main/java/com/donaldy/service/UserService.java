22bb3ce1208e04e269992e864347824470411a04
DonaldY <448641125@qq.com>
2018-10-17 15:08:20 +0800

    add service impl

--- src/main/java/com/donaldy/service/UserService.java
+++ src/main/java/com/donaldy/service/UserService.java
@@ -1,42 +1,16 @@
 package com.donaldy.service;
 
-import com.donaldy.handler.RestfulException;
 import com.donaldy.model.User;
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.validator.constraints.Length;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by DonaldY on 2018/7/11.
  */
-@Slf4j
-@Validated
-@Service
-public class UserService {
+public interface UserService {
 
-    @Async
-    public void executeUserInfo(Integer userId) {
-        log.info("enter executeUserInfo userId : {}, threadId : {}", userId, Thread.currentThread().getId());
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        log.info("exit executeUserInfo userId : {}, threadId : {}", userId, Thread.currentThread().getId());
-    }
+    public void executeUserInfo(Integer userId);
 
-    public List<User> getUserListByName(@NotBlank(message = "用户名不能为空")
-                                        @Length(min = 3, max = 10, message = "用户名长度大于3小于10") String username) {
-        System.out.println("username : " + username);
-        List<User> users = new ArrayList<>();
-        User user = new User();
-        users.add(user);
-        return users;
-    }
+    public List<User> getUserListByName(String username);
+
 }
---
+++ src/main/java/com/donaldy/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.donaldy.service.impl;
+
+import com.donaldy.model.User;
+import com.donaldy.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Validated
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Async
+    @Override
+    public void executeUserInfo(Integer userId) {
+        log.info("enter executeUserInfo userId : {}, threadId : {}", userId, Thread.currentThread().getId());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("exit executeUserInfo userId : {}, threadId : {}", userId, Thread.currentThread().getId());
+    }
+
+    @Override
+    public List<User> getUserListByName(@NotBlank(message = "用户名不能为空")
+                                        @Length(min = 3, max = 10, message = "用户名长度大于3小于10") String username) {
+        System.out.println("username : " + username);
+        List<User> users = new ArrayList<>();
+        User user = new User();
+        users.add(user);
+        return users;
+    }
+
+}
