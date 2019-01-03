a96da7509c7f43031a052b66c9373f6a59e13549
Donald <448641125@qq.com>
2019-01-03 19:33:00 +0800

    add page

---
+++ src/main/java/com/donaldy/utils/Page.java
@@ -0,0 +1,33 @@
+package com.donaldy.utils;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+public class Page<T> implements Serializable {
+    private static final long serialVersionUID = -1L;
+    private int page = 1;
+    private int pageSize = 10;
+    private int totalNum;
+    private int totalPage;
+    private List<T> list = Collections.emptyList();
+
+    public Page(int page, int pageSize, int totalNum, List<T> list) {
+        this.page = page;
+        this.pageSize = pageSize;
+        this.totalNum = totalNum;
+        this.list = list;
+    }
+
+    public Page() {
+    }
+
+    public Page(int total, List<T> list) {
+        this.totalNum = total;
+        this.list = list;
+    }
+
+}
