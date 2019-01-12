076895f05a7a5e3ebd60460b2a8a4a14715adefb
Donald <448641125@qq.com>
2019-01-12 23:21:24 +0800

    update

--- src/test/java/com/donaldy/unit/StreamTest.java
+++ src/test/java/com/donaldy/unit/StreamTest.java
@@ -1,6 +1,7 @@
 package com.donaldy.unit;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.stream.Stream;
 
 public class StreamTest {
@@ -13,6 +14,64 @@ public class StreamTest {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(generate(20).toArray()));
+        // System.out.println(Arrays.toString(generate(20).toArray()));
+
+        // 1
+        /*System.out.println(test("bbbbb"));
+
+        // 3
+        System.out.println(test("abc"));
+
+        // 3
+        System.out.println(test("abcabcabc"));
+*/
+        // 3
+        System.out.println(test("pwwkew"));
+
+        /*// 6
+        System.out.println(test("bbtablud"));*/
+    }
+
+    public static int test(String str) {
+
+        int count = 0;
+
+        int i = 0;
+
+        String test = "";
+
+        int temp = 0;
+
+        char [] arr = str.toCharArray();
+
+        for (int j = 0; j < arr.length; ++j) {
+
+            // System.out.println("out : " + test);
+
+            if (test.indexOf(arr[j] + "") < 0) {
+
+                //System.out.println("j : " + test.indexOf(arr[j] +""));
+
+                test += arr[j];
+
+                //System.out.println("test : " + test);
+
+                temp = test.length();
+
+                if (count < temp) {
+                    count = temp;
+                }
+
+                continue;
+            }
+
+            ;
+
+            test = str.substring(i - 1, j);
+
+            temp = 0;
+        }
+
+        return count;
     }
 }
