8e9b98111bbe80118a87ad67573f68af409e5a33
Donald <448641125@qq.com>
2019-01-25 15:12:44 +0800

    update

--- src/test/java/com/donaldy/unit/StreamTest.java
+++ src/test/java/com/donaldy/unit/StreamTest.java
@@ -1,5 +1,6 @@
 package com.donaldy.unit;
 
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class StreamTest {
@@ -8,6 +9,13 @@ public class StreamTest {
 
         System.out.println(123);
         return null;
+    }
+
+    public static void main(String[] args) {
+
+        // HashMap 先根据 hash 判断，再根据 equals
+        System.out.println(Objects.hash(1, 2, 2));
+        System.out.println(Objects.hash(2, 1, 2));
     }
 
 }
