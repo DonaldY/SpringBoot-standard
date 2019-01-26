fd1d4aa0ddf6891846d934e8a2b1906256c778e6
Donald <448641125@qq.com>
2019-01-26 23:46:28 +0800

    update

--- src/test/java/com/donaldy/unit/StreamTest.java
+++ src/test/java/com/donaldy/unit/StreamTest.java
@@ -1,5 +1,6 @@
 package com.donaldy.unit;
 
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -14,8 +15,14 @@ public class StreamTest {
     public static void main(String[] args) {
 
         // HashMap 先根据 hash 判断，再根据 equals
-        System.out.println(Objects.hash(1, 2, 2));
-        System.out.println(Objects.hash(2, 1, 2));
+        /*System.out.println(Objects.hash(1, 2, 2));
+        System.out.println(Objects.hash(2, 1, 2));*/
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.compute(1, (oldValue, newValue) -> newValue);
+
+        map.compute(1, (oldValue, newValue) -> newValue);
     }
 
 }
