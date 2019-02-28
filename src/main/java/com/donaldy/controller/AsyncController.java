a9992cad8c5657ab26e889841d34188d03aa46c3
Donald <448641125@qq.com>
2019-02-28 16:59:55 +0800

    add conditional property

--- src/main/java/com/donaldy/controller/AsyncController.java
+++ src/main/java/com/donaldy/controller/AsyncController.java
@@ -4,6 +4,7 @@ import com.donaldy.service.AsyncTask;
 import com.donaldy.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.Callable;
 
+@ConditionalOnProperty(name = "share.open", havingValue = "true")
 @Slf4j
 @RestController
 @RequestMapping("/async")
