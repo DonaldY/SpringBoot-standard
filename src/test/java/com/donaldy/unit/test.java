b4ac7c36bb448361134fc6c435b2aebdcf3fff6c
Donald <448641125@qq.com>
2019-04-03 23:42:59 +0800

    add test

---
+++ src/test/java/com/donaldy/unit/test.java
@@ -0,0 +1,17 @@
+package com.donaldy.unit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        for (String str : list) {
+
+            System.out.println(str);
+        }
+    }
+}
