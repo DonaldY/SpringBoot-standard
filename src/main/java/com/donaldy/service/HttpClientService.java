3c1d326948044c911a8edcb558aca07c7e6ed50e
Donald <448641125@qq.com>
2019-04-08 16:19:10 +0800

    update http client

--- src/main/java/com/donaldy/service/HttpClientService.java
+++ src/main/java/com/donaldy/service/HttpClientService.java
@@ -3,9 +3,10 @@ package com.donaldy.service;
 import com.donaldy.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class HttpClientService {
 
     @Autowired
