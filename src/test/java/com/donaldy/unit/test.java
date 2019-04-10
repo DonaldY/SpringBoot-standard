b208613495f3791d2141d8b90e63ba13b6c18070
Donald <448641125@qq.com>
2019-04-10 16:40:06 +0800

    update

--- src/test/java/com/donaldy/unit/test.java
+++ src/test/java/com/donaldy/unit/test.java
@@ -2,6 +2,8 @@ package com.donaldy.unit;
 
 import org.hashids.Hashids;
 
+import java.util.Arrays;
+
 public class test {
 
     public static void main(String[] args) {
@@ -9,5 +11,7 @@ public class test {
         Hashids hashids = new Hashids("Donald", 10);
 
         System.out.println(hashids.encode(221432423543535L));
+
+        System.out.println(Arrays.toString(hashids.decode("B9g7YNw0EX")));
     }
 }
