ef4f36c28b5bf0fd76f074e53bc1506aae66492d
Donald <448641125@qq.com>
2019-05-31 15:44:03 +0800

    update null test

--- src/test/java/com/donaldy/unit/NullTest.java
+++ src/test/java/com/donaldy/unit/NullTest.java
@@ -3,13 +3,26 @@ package com.donaldy.unit;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NullTest {
 
     public static void main(String[] args) {
 
         Test test = null;
 
-        System.out.println(test.getId());
+        //System.out.println(test.getId());
+
+        List<Integer> arr = new ArrayList<>();
+
+        if (arr.isEmpty()) {
+
+            return;
+        }
+
+        System.out.println(arr.get(0));
+
     }
 }
 
