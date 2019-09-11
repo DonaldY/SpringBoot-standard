ee9acc25f4a1636f64d045ace47586941805c846
Donald <448641125@qq.com>
2019-09-11 15:56:52 +0800

    add auto config test

--- src/main/java/com/donaldy/LearnApplication.java
+++ src/main/java/com/donaldy/LearnApplication.java
@@ -1,7 +1,11 @@
 package com.donaldy;
 
+import com.donaldy.service.UserService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
@@ -10,5 +14,19 @@ public class LearnApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
 	}
+
+	/*public static void main(String[] args) {
+
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(LearnApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+
+		UserService userService = context.getBean(UserService.class);
+
+		System.out.println("UserService Bean: " + userService);
+
+		// 关闭上下文
+		context.close();
+	}*/
 
 }
