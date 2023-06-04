783d14a7a17a2df23a2b2f8bdf11ab144914cfd5
yyf <448641125@qq.com>
2023-06-04 23:50:21 +0800

    feat: add digestUtils

--- src/test/java/com/donaldy/DigestUtilsExample.java
+++ src/test/java/com/donaldy/DigestUtilsExample.java
@@ -8,6 +8,11 @@ import org.apache.commons.codec.digest.DigestUtils;
  */
 public class DigestUtilsExample {
     public static void main(String[] args) {
+//        String sha1 = DigestUtils.sha1Hex(str); // 计算SHA-1散列值
+//        String sha256 = DigestUtils.sha256Hex(str); // 计算SHA-256散列值
+//        String sha384 = DigestUtils.sha384Hex(str); // 计算SHA-384散列值
+//        String sha512 = DigestUtils.sha512Hex(str); // 计算SHA-512散列值
+
         String str = "Hello, World!";
         String md5 = DigestUtils.md5Hex(str);
         System.out.println(md5);
