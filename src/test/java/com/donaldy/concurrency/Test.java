78481fc291d19378572d1e65532282f0edc56c42
yyf <448641125@qq.com>
2023-08-31 16:35:03 +0800

    feat: update test

---
+++ src/test/java/com/donaldy/concurrency/Test.java
@@ -0,0 +1,15 @@
+package com.donaldy.concurrency;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * @author donald
+ * @date 2023/8/31
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(StrUtil.join(" || ", "2323", "12"));
+    }
+}
