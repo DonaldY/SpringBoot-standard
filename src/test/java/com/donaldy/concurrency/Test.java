a68bec95ab67f869c5cc4eb73dd8ded49f346774
yyf <448641125@qq.com>
2023-08-31 17:34:15 +0800

    feat: update

--- src/test/java/com/donaldy/concurrency/Test.java
+++ src/test/java/com/donaldy/concurrency/Test.java
@@ -1,6 +1,14 @@
 package com.donaldy.concurrency;
 
-import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author donald
@@ -8,8 +16,18 @@ import cn.hutool.core.util.StrUtil;
  */
 public class Test {
 
-    public static void main(String[] args) {
+    @org.junit.Test
+    public void test3() throws IOException {
+        String json = getStr("/Users/yangyf/Documents/Code/java/SpringBoot-standard/src/main/resources/test.json");
+        List<Map<String, String>> listMap = JSONObject.parseObject(json, new TypeReference<List<Map<String, String>>>() {});
+        for (Map<String, String> map : listMap) {
+            System.out.println(map.getOrDefault("companyName", ""));
+        }
+    }
 
-        System.out.println(StrUtil.join(" || ", "2323", "12"));
+    static String getStr(String filePath) throws IOException {
+        FileInputStream fis = new FileInputStream(filePath);
+
+        return IOUtils.toString(fis, "UTF-8");
     }
 }
