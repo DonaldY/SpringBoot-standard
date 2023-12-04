6b6c42c883b2ed016f9290a28eee88f41536995e
yyf <448641125@qq.com>
2023-12-04 18:08:08 +0800

    feat: update

--- src/test/java/com/donaldy/concurrency/Test.java
+++ src/test/java/com/donaldy/concurrency/Test.java
@@ -20,13 +20,16 @@ public class Test {
     public void test3() throws IOException {
         String json = getStr("/Users/yangyf/Documents/Code/java/SpringBoot-standard/src/main/resources/test.json");
         List<Map<String, String>> listMap = JSONObject.parseObject(json, new TypeReference<List<Map<String, String>>>() {});
-        String sql = "INSERT INTO translation (tenantId, code, lang, translation) VALUES ('";
+        String sql = "INSERT IGNORE INTO translation (tenantId, code, lang, translation) VALUES ('";
         for (Map<String, String> map : listMap) {
             String tenantId = "flowmore";
             String code = map.getOrDefault("query", "");
             String lang = map.getOrDefault("targetLang", "");
             String translation = map.getOrDefault("target", "");
-            String realSql = sql + tenantId + "', '" + code + "', '" + lang + "', '" + translation + "')";
+            // 使用replaceAll方法将字符串中的单引号替换为两个单引号
+            code = code.replaceAll("'", "''");
+            translation = translation.replaceAll("'", "''");
+            String realSql = sql + tenantId + "', '" + code + "', '" + lang + "', '" + translation + "');";
             System.out.println(realSql);
         }
         // System.out.println(listMap.size());
