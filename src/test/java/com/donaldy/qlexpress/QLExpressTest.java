b43f2a6559f8530a26a6f75e8ceb6fa306f7e6b3
donald <448641125@qq.com>
2024-12-12 19:57:23 +0800

    feat: update qlexpress

--- src/test/java/com/donaldy/qlexpress/QLExpressTest.java
+++ src/test/java/com/donaldy/qlexpress/QLExpressTest.java
@@ -2,6 +2,7 @@ package com.donaldy.qlexpress;
 
 import com.ql.util.express.DefaultContext;
 import com.ql.util.express.ExpressRunner;
+import org.junit.Test;
 
 /**
  * @author donald
@@ -18,5 +19,23 @@ public class QLExpressTest {
         String express = "a + b * c";
         Object r = runner.execute(express, context, null, true, false);
         System.out.println(r);
+    }
+
+    @Test
+    public void test() throws Exception {
+        ExpressRunner runner = new ExpressRunner();
+        String expressQL = "function getUserInfo(int userId) {\r\n" +
+                "	if(userId > 0){\r\n" +
+                "		return \"获取到了用户的信息\"\r\n" +
+                "	}else{\r\n" +
+                "		return \"获取不到用户的信息\"\r\n" +
+                "	}\r\n" +
+                "}\r\n" +
+                "return getUserInfo(uid)";
+        DefaultContext<String, Object> params = new DefaultContext<>();
+        params.put("uid", 1);
+
+        Object result = runner.execute(expressQL, params, null, true, true);
+        System.out.println(result);
     }
 }
